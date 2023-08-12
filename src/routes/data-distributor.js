e8643e9964f1634966764a8a9d3436596c82de86
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-08-12 23:56:09 +0700

    add method to call specific store or all store

--- src/routes/data-distributor.js
+++ src/routes/data-distributor.js
@@ -12,6 +12,37 @@ const { MasterCourier } = require("../models/objects/master_courier");
 const InitDataDistributorRoute = (app) => {
 
     /*GET Method
+    * ROUTE: /{version}/store?id={id}
+    * This route fetch store info based on its id
+    * It can also call all store 
+    * TODO: give limit to the data requested
+    */
+    app.get(`/v${process.env.APP_MAJOR_VERSION}/stores`, checkAuth, async (req, res) => {
+        // check query param availability
+        if (!req.query) return res.status(400).send(UNIDENTIFIED_ERROR);
+
+        // DB request option declaration
+        const storeId = req.query.id;
+        const defaultOptions = {
+            status: ACTIVE
+        }
+        const options = storeId ? {
+            id: storeId,
+            ...defaultOptions
+        } : defaultOptions;
+
+        // DB request execution
+        await MasterStore.findAll({
+            where: options
+        }).then((result) => {
+            result = storeId ? result[0] : result;
+            return res.status(200).send(result);
+        }).catch((error) => {
+            SequelizeErrorHandling(error, res);
+        });
+    });
+
+    /*GET Method
     * ROUTE: /{version}/user/:id/stores
     * This route fetch all the user stores datasets
     */
--- src/routes/default.js
+++ src/routes/default.js
@@ -1,7 +1,7 @@
 
 const defaultRoute = (app) => {
     app.get(`/v${process.env.APP_MAJOR_VERSION}/`, (req, res) => {
-
+        return res.sendStatus(200);
     });
 }
 
