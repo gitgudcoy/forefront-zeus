4a99ca48c5d7d29d9deaf45ac7c07afc823d7507
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-08-21 15:54:00 +0700

    Update middleware.js

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -17,16 +17,12 @@ function handleCSRFToken(req, res, next) {
 
 async function checkAuth(req, res, next) {
   // Check the user session
-  console.log(JSON.stringify(req.headers));
-  console.log(req.headers[X_SID]);
   if (!req.headers[X_SID])
     return res.status(401).send(SESSION_TOKEN_NOT_FOUND);
 
   await sequelizeSessionStore.get(
     req.headers[X_SID],
     (err, res) => {
-      console.log("masuk");
-      console.log(err);
       if (err)
         return res
           .status(401)
