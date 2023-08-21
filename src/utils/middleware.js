ff5e8303f556075e728b27bb704bc2ea8d9d9d3e
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-08-21 15:26:10 +0700

    add sid to the app

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -4,6 +4,10 @@ const {
   USER_UNAUTHORIZED,
   PLEASE_VERIFY_OTP,
 } = require("../variables/responseMessage");
+const { X_SID } = require("../variables/general");
+const {
+  sequelizeSessionStore,
+} = require("../config/sequelize");
 
 function handleCSRFToken(req, res, next) {
   console.log("masuk sini pakcik");
@@ -13,26 +17,35 @@ function handleCSRFToken(req, res, next) {
 
 async function checkAuth(req, res, next) {
   // Check the user session
-  if (!req.session) return res.sendStatus(401);
-  if (!req.session.refreshTokens)
+  if (!req.headers[X_SID])
     return res.status(401).send(SESSION_TOKEN_NOT_FOUND);
 
-  // Check the JWT in the header
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token === null)
-    return res.status(401).send(USER_UNAUTHORIZED);
+  await sequelizeSessionStore.get(
+    req.headers[X_SID],
+    (err, res) => {
+      if (err)
+        return res
+          .status(401)
+          .send(SESSION_TOKEN_NOT_FOUND);
 
-  // Verify JWT access token
-  jwt.verify(
-    token,
-    process.env.APP_ACCESS_TOKEN_SECRET,
-    (err, user) => {
-      if (err) return res.status(500).send(err);
-      if (!user.OTPVerified)
-        return res.status(403).send(PLEASE_VERIFY_OTP);
-      req.user = user;
-      next();
+      // Check the JWT in the header
+      const authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
+      if (token === null)
+        return res.status(401).send(USER_UNAUTHORIZED);
+
+      // Verify JWT access token
+      jwt.verify(
+        token,
+        process.env.APP_ACCESS_TOKEN_SECRET,
+        (err, user) => {
+          if (err) return res.status(500).send(err);
+          if (!user.OTPVerified)
+            return res.status(403).send(PLEASE_VERIFY_OTP);
+          req.user = user;
+          next();
+        }
+      );
     }
   );
 }
